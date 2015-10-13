f0868d5f04812c27884196392cfbe52ad0da9d10
Cindy Leschaud <cindy.leschaud@gmail.com>
2015-10-13 19:45:27 +0200

    add some details about me

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -15,10 +15,17 @@
                         <img height="100px" class="thumbnail" style="margin: 0 auto;" src="{{ asset('frontend/images/cindy.jpg') }}" />
                     </p>
                     <h3 class="text-info">Conceptrice Multimédia et Web Développeur</h3>
+                    <ul class="m-t-lg m-b-xl">
+                        <li class="text-danger">
+                            <h4><i class="fa fa-gear"></i> &nbsp; Geek, nerd, chocolat et Laravel aficionado  &nbsp; <i class="fa fa-gear"></i></h4>
+                        </li>
+                    </ul>
                     <p class="m-t-xl m-b-xl">
-                        Je travaille en tant que développeur web pour la Faculté de droit de l'Université de Neuchâtel et en tant qu'indépendante
-                        pour divers clients, petites entreprises et associations sur des projets de toutes tailles.
+                        Développeur web et chef de projet pour la <a href="http://unine.ch/droit.ch">Faculté de droit de l'Université de Neuchâtel</a> <br/>
+                        Indépendante pour divers clients, petites entreprises et associations sur des projets de toutes tailles.
                     </p>
+                    <p class="m-t-lg m-b-xl">Spécialisée en développement d'applications web en PHP/MySQL.</p>
+
                 </div>
 
             </div>
@@ -29,9 +36,7 @@
         <div class="container">
             <div class="row m-t-xl text-center">
                 <h3 class="m-t-none text-uc">Quelques réalisations</h3>
-
                 <h4 class="m-t-xl"><i class="fa fa-code fa-2x text-info"></i></h4>
-                <p class="m-t-lg">Développement d'applications web et CMS en PHP/MySQL et/ou Javascript.</p>
 
             </div>
             <div class="row">
@@ -64,12 +69,12 @@
                 </div>
                 <div class="col-sm-3 text-center wrapper-sm">
                     <p class="m-b-lg m-t-lg">
-                        <a class="btn btn-info btn-block" target="_blank" href="http://www.janohair.ch"><i class="fa fa-link"></i> www.janohair.ch</a>
+                        <a class="btn btn-info btn-block" target="_blank" href="http://www.alumnine-droit.ch"><i class="fa fa-link"></i> www.alumnine-droit.ch</a>
                     </p>
                 </div>
                 <div class="col-sm-3 text-center wrapper-sm">
                     <p class="m-b-lg m-t-lg">
-                        <a class="btn btn-info btn-block" target="_blank" href="http://www.cie-anadyomene.com"><i class="fa fa-link"></i> www.cie-anadyomene.com</a>
+                        <a class="btn btn-info btn-block" target="_blank" href="http://www.janohair.ch"><i class="fa fa-link"></i> www.janohair.ch</a>
                     </p>
                 </div>
                 <div class="col-sm-3 text-center wrapper-sm">
